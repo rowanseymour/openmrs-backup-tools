8e3a0e5753956b558103b5148a0c823b9f1ee1f7
Rowan Seymour <rowanseymour@gmail.com>
2012-02-24 14:27:22 +0200

    Initial commit

---
+++ openmrs_backup.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+#################### Config #####################
+
+# The OpenMRS runtime properties file
+OPENMRS_PROP_FILE="/usr/share/tomcat6/.OpenMRS/openmrs-runtime.properties"
+
+# Destination directory for backups
+BACKUP_DEST_DIR="/home/emr-admin/openmrs_backups"
+
+# Number of days to keep daily dumps before deletion (0 = never delete)
+DAILY_KEEP_DAYS=14
+
+# Number of weeks to keep weekly dumps before deletion (0 = never delete)
+WEEKLY_KEEP_WEEKS=6
+
+# Number of months to keep monthly dumps before deletion (0 = never delete)
+MONTHLY_KEEP_MONTHS=12
+
+# Logging tag
+LOGGING_TAG="OPENMRS-BACKUP"
+
+#################### Functions #####################
+
+# Fail function to record error in syslog
+fail() {
+	logger -t $LOGGING_TAG -p local0.crit $1
+	echo $1
+	exit
+}
+
+#################### Main #####################
+
+# Check runtime properties file exists
+if ! [ -e "$OPENMRS_PROP_FILE" ]; then
+	fail "Specified OpenMRS runtime properties file does not exist"
+fi
+
+# Read properties from properties file
+dbuser=`sed '/^\#/d' "$OPENMRS_PROP_FILE" | grep 'connection.username' | tail -n 1 | cut -d "=" -f2-`
+dbpass=`sed '/^\#/d' "$OPENMRS_PROP_FILE" | grep 'connection.password' | tail -n 1 | cut -d "=" -f2-`
+dburl=`sed '/^\#/d' "$OPENMRS_PROP_FILE" | grep 'connection.url' | tail -n 1 | cut -d "=" -f2-`
+
+# Check properties could be read
+if [ -z $dbuser ] || [ -z $dbpass ] || [ -z $dburl ]; then
+	fail "Unable to read OpenMRS runtime properties"
+fi
+
+# Extract database name from connection URL
+if [[ $dburl =~ /([a-zA-Z0-9_\-]+)\? ]]; then
+        dbname=${BASH_REMATCH[1]}
+else
+        dbname="openmrs"
+fi
+
+# Check destination directory exists and is writable
+if ! [ -d "$BACKUP_DEST_DIR" ] || ! [ -w "$BACKUP_DEST_DIR" ]; then
+	fail "Backup destination directory does not exist or is not writable"
+fi
+
+# Determine whether this is daily/weekly/monthly based on current date
+dayofmonth=`date +%d` # 1-31
+dayofweek=`date +%u` # 1-7 (Monday-Sunday)
+
+if [ dayofmonth = "1" ]; then
+	dumpfilename="`date '+%Y-%m-%d'`-openmrsdb-monthly.gz"
+elif [ dayofweek = "1" ]; then
+	dumpfilename="`date '+%Y-%m-%d'`-openmrsdb-weekly.gz"
+else
+	dumpfilename="`date '+%Y-%m-%d'`-openmrsdb-daily.gz"
+fi 
+
+dumpfile="$BACKUP_DEST_DIR/$dumpfilename"
+
+# Delete dump file if it already exists
+if [ -e $dumpfile ]; then
+	rm $dumpfile
+fi
+
+# Dump OpenMRS database and gzip result
+mysqldump -u$dbuser -p$dbpass $dbname | gzip -c > $dumpfile
+
+# Check dump was successful and new dump file exists
+if [ ${PIPESTATUS[0]} -eq 0 ] && [ -e $dumpfile ]; then
+	logger -t $LOGGING_TAG -p local0.info "Database dump successful"
+else
+	fail "Unable to dump database (name=$dbname, user=$dbuser)"
+fi
+
+# Optionally delete old daily dumps
+if [ $DAILY_KEEP_DAYS -ne 0 ]; then
+	find "$BACKUP_DEST_DIR" -name "*-daily.gz" -type f -mtime +${DAILY_KEEP_DAYS} -delete
+fi
+
+# Optionally delete old weekly dumps
+if [ $WEEKLY_KEEP_WEEKS -ne 0 ]; then
+	keepdays=`expr $((($WEEKLY_KEEP_WEEKS * 7) + 1))`
+	find "$BACKUP_DEST_DIR" -name "*-weekly.gz" -type f -mtime +${keepdays} -delete
+fi
+
+# Optionally delete old monthly dumps
+if [ $MONTHLY_KEEP_MONTHS -ne 0 ]; then
+	keepdays=`expr $((($MONTHLY_KEEP_MONTHS * 31) + 1))`
+	find "$BACKUP_DEST_DIR" -name "*-monthly.gz" -type f -mtime +${keepdays} -delete
+fi
